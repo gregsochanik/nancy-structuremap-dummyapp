cfbc492dfa0eed4a20969148731902324d7ad3eb
greg Sochanik <greg@sochanik.com>
2014-06-11 11:02:25 +0100

    Remove summary from overriden method

--- NancyStructureMap3Service/StructureMapNancyBootstrapper_OverriddenGetModule.cs
+++ NancyStructureMap3Service/StructureMapNancyBootstrapper_OverriddenGetModule.cs
@@ -7,12 +7,6 @@ namespace NancyStructureMap3Service
 {
 	public abstract class StructureMapNancyBootstrapper_OverriddenGetModule : StructureMapNancyBootstrapper
 	{
-		/// <summary>
-		/// Retreive a specific module instance from the container
-		/// </summary>
-		/// <param name="container">Container to use</param>
-		/// <param name="moduleType">Type of the module</param>
-		/// <returns>A <see cref="INancyModule"/> instance</returns>
 		protected override INancyModule GetModule(IContainer container, Type moduleType)
 		{
 			return (INancyModule)container.GetInstance(moduleType);
