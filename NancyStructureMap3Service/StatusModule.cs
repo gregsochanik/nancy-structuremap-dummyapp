67ccbc34da8fbeb2ea729729a8e1e48702d2cd73
greg Sochanik <greg@sochanik.com>
2014-09-24 12:31:11 +0100

    Added test ctor injected class

--- NancyStructureMap3Service/StatusModule.cs
+++ NancyStructureMap3Service/StatusModule.cs
@@ -1,14 +1,32 @@
 ﻿using Nancy;
-using Nancy.Bootstrapper;
 
 namespace NancyStructureMap3Service
 {
 	public class StatusModule : NancyModule
 	{
-		public StatusModule()
+		private readonly ITestRepo _testRepo;
+		
+		public StatusModule(ITestRepo testRepo)
 		{
+			_testRepo = testRepo;
+
+			Get["/test"] = _ => _testRepo.GetSomeThing();
 			Get["/status"] = _ => "OK";
 			Get["/status/{id}"] = _ => "OK " + _.id;
 		}
 	}
+
+	public interface ITestRepo
+	{
+		string GetSomeThing();
+	}
+
+	public class TestRepo : ITestRepo
+	{
+		public string GetSomeThing()
+		{
+			return "Hello world!!";
+		}
+	}
+
 }
---
+++ NancyStructureMap3Service/Tests/Bootstrap_ctor_collaborators_check.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Http;
+using NUnit.Framework;
+
+namespace Tests
+{
+	[TestFixture]
+	public class Bootstrap_ctor_collaborators_check
+	{
+		private const string TEST_SERVICE_URI = "http://localhost/NancyStructureMap3Service/test";
+
+		[Test]
+		public async void Service_can_resolve_concrete_from_interface()
+		{
+			var httpClient = new HttpClient();
+			var response = await httpClient.GetAsync(TEST_SERVICE_URI);
+			var body = await response.Content.ReadAsStringAsync();
+
+			Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+			Assert.That(body, Is.EqualTo("Hello world!!"));
+		}
+	}
+}
